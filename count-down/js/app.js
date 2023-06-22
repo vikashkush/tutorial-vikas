bb7c380ad3c1599a8fb98563d0cc10b229a84ddf
vikaskush <vikashkushwah9759@gmail.com>
2023-06-22 13:36:22 +0530

    today

---
+++ count-down/js/app.js
@@ -0,0 +1,11 @@
+const endDate="12 june 2023 10:00AM"
+document.getElementById("end-date").innerText=endDate;
+
+function clock()
+{
+    const end=  (endDate)
+    const now=new Date()
+console.log(end);
+console.log(now);
+}
+ clock()
